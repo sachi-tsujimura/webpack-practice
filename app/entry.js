d95a0c21b931b887a22b528a90569b1a178c06f0
Sachi Tsujimura <54939119+sachi-tsujimura@users.noreply.github.com>
2020-11-25 00:16:26 +0900

    first commit

---
+++ app/entry.js
@@ -0,0 +1,9 @@
+'use strict';
+const  $ = require('jquery');
+const  block = $('#block');
+const  scalingButton = $('#scaling-button');
+
+scalingButton.click(() => {
+  block.animate({ width: '200pt', height: '200pt' }, 2000);
+  block.animate({ width: '100pt', height: '100pt' }, 2000);
+});
